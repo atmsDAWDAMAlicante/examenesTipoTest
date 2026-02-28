287291da3b9721b25a4f22cd2bea412bc30df190
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-02-28 19:15:39 +0100

    App web para hacer exámenes tipo test

---
+++ test.js
@@ -0,0 +1,19 @@
+// Array de objetos con las preguntas y respuestas de ejemplo
+
+const preguntas = [
+            {
+                pregunta: "¿Cuál es la capital de Francia?",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["París", "Londres", "Madrid", "Berlín"]
+            },
+            {
+                pregunta: "¿Qué lenguaje se usa para programar en la web?",
+                idCorrecta: 2,
+                respuestas: ["Python", "Java", "JavaScript", "C++"]
+            },
+            {
+                pregunta: "¿Cuántos planetas tiene el sistema solar?",
+                idCorrecta: 3,
+                respuestas: ["7", "9", "10", "8"]
+            }
+        ];
