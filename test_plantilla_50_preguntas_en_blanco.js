ddc01b97613eb5d15a0c2272170b19fe5e4603a9
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-03-01 13:27:25 +0100

    Versión para que funcionen en Android y plantilla de test.js

---
+++ test_plantilla_50_preguntas_en_blanco.js
@@ -0,0 +1,255 @@
+// Array de objetos con 50 preguntas y respuestas para rellenar
+
+const preguntas = [
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            },
+            {
+                pregunta: "escribe la pregunta",
+                idCorrecta: 0, // Índice de la respuesta correcta
+                respuestas: ["una", "dos", "tres", "cuatro"]
+            }	
+			
+        ];
